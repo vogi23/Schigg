3a3238d33b2751947419ee8e9aa76d6108702edf
vogi23 <chrigu@vgbau.ch>
2015-04-17 12:30:42 +0200

    First commit ever

---
+++ app/src/main/java/ISchigg.java
@@ -0,0 +1,12 @@
+/**
+ * Created by tgdvoch5 on 17.04.2015.
+ */
+public interface ISchigg {
+    int getID();
+    String getWort();
+    void setWort(String wort);
+    String getBeschribig();
+    void setBeschribig(String beschribig);
+    String getPLZ();
+    void setPLZ(String plz);
+}
---
+++ app/src/main/java/Schigg.java
@@ -0,0 +1,5 @@
+/**
+ * Created by tgdvoch5 on 17.04.2015.
+ */
+public class Schigg implements ISchigg{
+}
